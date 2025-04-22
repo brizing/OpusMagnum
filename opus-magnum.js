5b9e39d1ddddcd81ec3f97f6cc7e924df2f7d73b
Serhii Brizing <ser.artuh@gmail.com>
2025-04-22 15:29:56 +0300

    Expr cacheLength

--- opus-magnum.js
+++ opus-magnum.js
@@ -595,3 +595,4 @@ var matches = documentElement.matches || documentElement.msMatchesSelector;
 /**
  * Create key-value caches of limited size
  * @returns {function(string, object)} Returns the Object data after storing it on itself with
+ *	property name the (space-suffixed) string and (if the cache is larger than Expr.cacheLength)
