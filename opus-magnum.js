00c279f2ba74459b9bc9d3b146dba7890dfd13a3
Serhii Brizing <ser.artuh@gmail.com>
2026-02-27 15:29:41 +0200

    such contains

--- opus-magnum.js
+++ opus-magnum.js
@@ -561,3 +561,4 @@ if ( !support.cssHas ) {
 
 	// Support: Chrome 105 - 110+, Safari 15.4 - 16.3+
 	// Our regular `try-catch` mechanism fails to detect natively-unsupported
+	// pseudo-classes inside `:has()` (such as `:has(:contains("Foo"))`)
