07214e0d5321a4e8b2835c54e3c6678760c452e8
Serhii Brizing <ser.artuh@gmail.com>
2026-03-09 15:29:42 +0200

    identifier whitespace

--- opus-magnum.js
+++ opus-magnum.js
@@ -625,3 +625,4 @@ function testContext( context ) {
 }
 
 // Attribute selectors: https://www.w3.org/TR/selectors/#attribute-selectors
+var attributes = "\\[" + whitespace + "*(" + identifier + ")(?:" + whitespace +
