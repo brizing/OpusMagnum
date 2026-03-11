60f3e79e05de2cf0893cf89cfbc60877be8e422e
Serhii Brizing <ser.artuh@gmail.com>
2026-03-11 11:53:15 +0200

    ☕️

--- opus-magnum.js
+++ opus-magnum.js
@@ -638,3 +638,4 @@ var pseudos = ":(" + identifier + ")(?:\\((" +
 
 	// To reduce the number of selectors needing tokenize in the preFilter, prefer arguments:
 	// 1. quoted (capture 3; capture 4 or capture 5)
+	"('((?:\\\\.|[^\\\\'])*)'|\"((?:\\\\.|[^\\\\\"])*)\")|" +
