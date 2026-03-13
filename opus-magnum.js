9c2a4b297219988dea9050fae95765e05e876e61
Serhii Brizing <ser.artuh@gmail.com>
2026-03-13 10:05:13 +0200

    slice 0x10000

--- opus-magnum.js
+++ opus-magnum.js
@@ -666,3 +666,4 @@ var rpseudo = new RegExp( pseudos );
 var runescape = new RegExp( "\\\\[\\da-fA-F]{1,6}" + whitespace +
 	"?|\\\\([^\\r\\n\\f])", "g" ),
 	funescape = function( escape, nonHex ) {
+		var high = "0x" + escape.slice( 1 ) - 0x10000;
