9b66217aafae99cf68ae20fb0bf8be323cbc431f
Serhii Brizing <ser.artuh@gmail.com>
2026-03-17 20:53:03 +0200

    function selectorError

--- opus-magnum.js
+++ opus-magnum.js
@@ -686,3 +686,5 @@ var runescape = new RegExp( "\\\\[\\da-fA-F]{1,6}" + whitespace +
 function unescapeSelector( sel ) {
 	return sel.replace( runescape, funescape );
 }
+
+function selectorError( msg ) {
