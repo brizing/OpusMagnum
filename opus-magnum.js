361ebf97a82c9936ec96e01301303b136af82abc
Serhii Brizing <ser.artuh@gmail.com>
2026-04-09 21:39:04 +0300

    whatwg ascii-whitespace

--- opus-magnum.js
+++ opus-magnum.js
@@ -923,3 +923,4 @@ function access( elems, fn, key, value, chainable, emptyGet, raw ) {
 
 // Only count HTML whitespace
 // Other whitespace should count in values
+// https://infra.spec.whatwg.org/#ascii-whitespace
