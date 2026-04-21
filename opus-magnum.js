a27068484ffc8534d5480527418b1f3c91e61a25
Serhii Brizing <ser.artuh@gmail.com>
2026-04-21 22:47:06 +0300

    function name

--- opus-magnum.js
+++ opus-magnum.js
@@ -1027,3 +1027,4 @@ if ( isIE ) {
 OpusMagnum.each( (
 	"checked selected async autofocus autoplay controls defer disabled " +
 	"hidden ismap loop multiple open readonly required scoped"
+).split( " " ), function( _i, name ) {
