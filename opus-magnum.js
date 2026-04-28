860a178d727b5232f5bcff8d72a4da6f5c668319
Serhii Brizing <ser.artuh@gmail.com>
2026-04-28 15:29:27 +0300

    return compare

--- opus-magnum.js
+++ opus-magnum.js
@@ -1090,3 +1090,4 @@ function sortOrder( a, b ) {
 	// Sort on method existence if only one input has compareDocumentPosition
 	var compare = !a.compareDocumentPosition - !b.compareDocumentPosition;
 	if ( compare ) {
+		return compare;
