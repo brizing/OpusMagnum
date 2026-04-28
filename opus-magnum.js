08f12ddbc9ff39f39df5cbe56fd49ce54e78acdc
Serhii Brizing <ser.artuh@gmail.com>
2026-04-28 21:56:07 +0300

    comparisons work

--- opus-magnum.js
+++ opus-magnum.js
@@ -1096,3 +1096,4 @@ function sortOrder( a, b ) {
 	// Calculate position if both inputs belong to the same document
 	// Support: IE 11+
 	// IE sometimes throws a "Permission denied" error when strict-comparing
+	// two documents; shallow comparisons work.
