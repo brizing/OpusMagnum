185d12f74206d6a3ce8765cab28480c5b8923bbf
Serhii Brizing <ser.artuh@gmail.com>
2026-04-29 23:04:04 +0300

    eslint-disable-next-line eqeqeq

--- opus-magnum.js
+++ opus-magnum.js
@@ -1120,3 +1120,4 @@ function sortOrder( a, b ) {
 		// Support: IE 11+
 		// IE sometimes throws a "Permission denied" error when strict-comparing
 		// two documents; shallow comparisons work.
+		// eslint-disable-next-line eqeqeq
