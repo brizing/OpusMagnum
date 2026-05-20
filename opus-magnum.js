46baf7a9639f8a5b2e00d23541503aa69f429435
Serhii Brizing <ser.artuh@gmail.com>
2026-05-20 17:17:05 +0300

    test selector

--- opus-magnum.js
+++ opus-magnum.js
@@ -1294,3 +1294,4 @@ function find( selector, context, results, seed ) {
 					( rdescend.test( selector ) || rleadingCombinator.test( selector ) ) ) {
 
 					// Expand context for sibling selectors
+					newContext = rsibling.test( selector ) &&
