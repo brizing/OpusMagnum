90e56432975b79bdeb20721b02072471e841cdfc
Serhii Brizing <ser.artuh@gmail.com>
2026-06-10 02:11:13 +0300

    when strict-comparing

--- opus-magnum.js
+++ opus-magnum.js
@@ -1461,3 +1461,4 @@ function setDocument( node ) {
 
 	// Return early if doc is invalid or already selected
 	// Support: IE 11+
+	// IE sometimes throws a "Permission denied" error when strict-comparing
