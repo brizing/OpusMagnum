a24cbbc9eef054d604ccce77974ac24c4227bf2a
Serhii Brizing <ser.artuh@gmail.com>
2026-06-11 15:29:53 +0300

    eslint-disable-next-line eqeqeq

--- opus-magnum.js
+++ opus-magnum.js
@@ -1478,3 +1478,4 @@ function setDocument( node ) {
 	// Support: IE 11+
 	// IE sometimes throws a "Permission denied" error when strict-comparing
 	// two documents; shallow comparisons work.
+	// eslint-disable-next-line eqeqeq
