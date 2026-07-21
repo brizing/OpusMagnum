a903cc181219ec077de0913e9bc679f142b1b3d2
Serhii Brizing <ser.artuh@gmail.com>
2026-07-21 23:38:21 +0300

    factory OpusMagnum

--- opus-magnum.js
+++ opus-magnum.js
@@ -5,3 +5,4 @@
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
 
 		// For CommonJS and CommonJS-like environments where a proper `window`
+		// is present, execute the factory and get OpusMagnum.
