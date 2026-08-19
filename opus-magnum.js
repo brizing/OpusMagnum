0695ada5c5fe09d3298eb39fb265e87e41ad2230
Serhii Brizing <ser.artuh@gmail.com>
2026-08-20 00:46:05 +0300

    Ctor ObjectFunctionString

--- opus-magnum.js
+++ opus-magnum.js
@@ -309,3 +309,4 @@ OpusMagnum.extend( {
 
 		// Objects with prototype are plain iff they were constructed by a global Object function
 		Ctor = hasOwn.call( proto, "constructor" ) && proto.constructor;
+		return typeof Ctor === "function" && fnToString.call( Ctor ) === ObjectFunctionString;
